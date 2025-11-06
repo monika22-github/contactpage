822abdaf059cb31617642057410e57569a0d756f
Monika verma <mailme.monika14@gmail.com>
2025-11-06 14:41:45 +0530

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+
+      const Nav = () => {
+
+    
+        
+         return (
+           <div className="main text-center p-4 bg-gray-200">
+        
+            <nav>
+            <a href="">CONTACT</a>
+            <a href="">ABOUT</a>
+           <a href="">HOME</a>
+             
+
+                
+
+            </nav>
+ 
+
+        </div>
+    )
+}
+
+export default Nav;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Nav from './components/Nav.jsx'
+import Contact from './components/Contact.jsx'
+import './index.css'
+import './All.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Nav />
+     {/* <Contact />  */}
+
+  </React.StrictMode>,
+)
